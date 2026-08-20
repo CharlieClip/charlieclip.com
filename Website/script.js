2f4e36c92b62b988be231da9e756a516cbf7f626
CharlieClip <deskcharlie2@gmail.com>
2026-08-20 17:33:02 -0400

    Delete Website directory

--- Website/script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("testButton");
-const message = document.getElementById("message");
-
-button.addEventListener("click", function () {
-    message.textContent = "The button works!";
-});
